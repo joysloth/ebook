393f057c012176f85a969f59e92ac9d291ba896f
anhojin <anhj930@naver.com>
2019-05-29 10:55:40 +0900

    오른쪽 스마일메뉴 올림

--- js/login.js
+++ js/login.js
@@ -5,4 +5,22 @@ $(document).ready(function(){
     $('.b img').on('mouseout', function(){
        $(this).css('transform', '').css('border-radius','30px');
     });
+    $('.login').on("mouseover", function(){
+        $(this).css('background-color', '#16a085');
+    });
+     $('.login').on("mouseout", function(){
+        $(this).css('background-color', 'white');
+    });
+    $('.smile').on("mouseover", function(){
+        $(this).css('background-color', '#16a085');
+    });
+     $('.smile').on("mouseout", function(){
+        $(this).css('background-color', 'white');
+    });
+    $('.home').on("mouseover", function(){
+        $(this).css('background-color', '#16a085');
+    });
+     $('.home').on("mouseout", function(){
+        $(this).css('background-color', 'white');
+    });
     });
